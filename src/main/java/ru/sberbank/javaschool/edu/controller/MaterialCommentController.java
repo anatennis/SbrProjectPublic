ec535dc6b97a0c910ac8bdb90a5345c48ac96d9a
nick <nick.targon@gmail.com>
2019-07-15 15:23:54 +0300

    material comments delete and editing

--- src/main/java/ru/sberbank/javaschool/edu/controller/MaterialCommentController.java
+++ src/main/java/ru/sberbank/javaschool/edu/controller/MaterialCommentController.java
@@ -3,11 +3,15 @@ package ru.sberbank.javaschool.edu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.sberbank.javaschool.edu.domain.Material;
 import ru.sberbank.javaschool.edu.domain.MaterialComment;
 import ru.sberbank.javaschool.edu.domain.User;
+import ru.sberbank.javaschool.edu.repository.MaterialCommentRepository;
 import ru.sberbank.javaschool.edu.repository.MaterialRepository;
 import ru.sberbank.javaschool.edu.service.MaterialCommentService;
 
@@ -15,11 +19,17 @@ import ru.sberbank.javaschool.edu.service.MaterialCommentService;
 public class MaterialCommentController {
     private final MaterialRepository materialRepository;
     private final MaterialCommentService commentService;
+    private final MaterialCommentRepository materialCommentRepository;
 
     @Autowired
-    public MaterialCommentController(MaterialRepository materialRepository, MaterialCommentService commentService) {
+    public MaterialCommentController(
+            MaterialRepository materialRepository,
+            MaterialCommentService commentService,
+            MaterialCommentRepository materialCommentRepository
+    ) {
         this.materialRepository = materialRepository;
         this.commentService = commentService;
+        this.materialCommentRepository = materialCommentRepository;
     }
 
     @PostMapping("/course/{idCourse}/comment/{idMaterial}")
@@ -29,6 +39,49 @@ public class MaterialCommentController {
         Material material = materialRepository.getMaterialById(idMaterial);
 
         commentService.addComment(material, user, comment);
+
+        return "redirect:/course/{idCourse}";
+    }
+
+    @DeleteMapping("/course/{idCourse}/delete_comment/{idComment}")
+    public String deleteComment(
+            @PathVariable Long idComment,
+            @AuthenticationPrincipal User user
+    ) {
+        boolean result = commentService.deleteComment(user, idComment);
+        System.out.println("------>" + result);
+
+        return "redirect:/course/{idCourse}";
+
+    }
+
+    @GetMapping("/course/{idCourse}/edit_comment/{idComment}")
+    public String showEditingComment(
+            @PathVariable long idCourse,
+            @PathVariable long idComment,
+            @AuthenticationPrincipal User user,
+            Model model
+    ) {
+
+        MaterialComment comment = materialCommentRepository.findMaterialCommentById(idComment);
+
+        if (!commentService.canEditComment(user, comment)) {
+            return "redirect:/course/{idCourse}";
+        }
+
+        model.addAttribute("comment", comment);
+        model.addAttribute("idCourse", idCourse);
+
+        return "material_comment_edit";
+    }
+
+    @PostMapping("/course/{idCourse}/edit_comment/{idComment}")
+    public String editMaterial(
+            @PathVariable Long idComment,
+            MaterialComment comment,
+            @AuthenticationPrincipal User user
+    ) {
+        commentService.editComment(idComment, comment, user);
 
         return "redirect:/course/{idCourse}";
     }
--- src/main/java/ru/sberbank/javaschool/edu/service/MaterialCommentService.java
+++ src/main/java/ru/sberbank/javaschool/edu/service/MaterialCommentService.java
@@ -26,4 +26,34 @@ public class MaterialCommentService {
 
         commentRepository.save(materialComment);
     }
+
+    public boolean canEditComment(User user, MaterialComment comment) {
+
+        return comment.getAuthor().equals(user);
+    }
+
+    public boolean deleteComment(User user, long id) {
+        MaterialComment comment = commentRepository.findMaterialCommentById(id);
+
+        if (comment == null || !canEditComment(user, comment)) {
+            return false;
+        }
+
+        commentRepository.delete(comment);
+
+        return true;
+    }
+
+    public boolean editComment(long id, MaterialComment comment, User user) {
+        MaterialComment commentFromDb = commentRepository.findMaterialCommentById(id);
+
+        if (commentFromDb == null || !canEditComment(user, commentFromDb)) {
+            return false;
+        }
+
+        commentFromDb.setText(comment.getText());
+        commentRepository.save(commentFromDb);
+
+        return true;
+    }
 }
